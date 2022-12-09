8bf4237c8011084fd2eb2d00e91360af302812fb
christovanqueiroz <christovanbrumqueiroz@gmail.com>
2022-12-09 17:20:47 -0300

    modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 const body = document.querySelector('body')
-const switcher = document.querySelectorAll('.switcher div')
 const button = document.querySelector('.button')
 const moon = document.querySelector('.moon');
 const sun = document.querySelector('.sun');
 
-switcher.forEach(btn => {
-    btn.addEventListener('click', () => {
-        body.classList.toggle('light')
-    })
-})
-
 sun.addEventListener('click', () => {
     button.style.left = "51%";
-    sun.classList.remove("hide");
-    moon.classList.add("hide");
+    body.classList.toggle('light')
 })
 
 moon.addEventListener('click', () => {
     button.style.left = "42%";
-    moon.classList.remove("hide");
-    sun.classList.add("hide")
+    body.classList.toggle('light')
 })
-
-console.log(sun)
